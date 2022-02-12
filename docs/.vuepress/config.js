536646e47e35237047e2b084092a3b08baf3a773
feiben <feiben.dev@gmail.com>
2022-02-12 16:57:46 +0800

    fix theme-color

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,7 @@ module.exports = {
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ['link', { rel: 'icon', href: '/imgs/logo.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: 'transparent' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['link', { rel: 'apple-touch-icon', href: '/imgs/logo.png' }],
